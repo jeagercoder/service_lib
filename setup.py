3ffc427024566333ac2c537965cb7a8cac065412
Jeager Coder <61510810+jeagercoder@users.noreply.github.com>
2023-04-06 16:41:38 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='service_lib',
-    version='0.0.2',
+    version='0.0.3',
     license='BSD',
     description='Library dependency microservice.',
     long_description_content_type='text/markdown',
